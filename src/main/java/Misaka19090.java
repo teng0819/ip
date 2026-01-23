2789702c4b488fa9aa28883c9ac4a4f1097a0f7f
Teng Lingjing <e1354629@u.nus.edu>
2026-01-23 17:36:04 +0800

    Level-3: Mark as done

--- src/main/java/Misaka19090.java
+++ src/main/java/Misaka19090.java
@@ -1,9 +1,30 @@
 import java.util.Scanner;
+class Task {
+    String description;
+    boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    public String toString() {
+        return (isDone ? "[X] " : "[ ] ") + description;
+    }
+}
 public class Misaka19090 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         System.out.println("____________________________________________________________");
@@ -21,12 +42,30 @@ public class Misaka19090 {
                 break;
             }
             if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
                 continue;
             }
-            tasks[taskCount] = input;
+
+            if (input.startsWith("mark ")) {
+                int idx = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks[idx].mark();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(" " + tasks[idx]);
+                continue;
+            }
+
+            if (input.startsWith("unmark ")) {
+                int idx = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks[idx].unmark();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(" " + tasks[idx]);
+                continue;
+            }
+
+            tasks[taskCount] = new Task(input);
             taskCount++;
 
             System.out.println("____________________________________________________________");
