0a8607bed31c400d1d0dd70531d1b4286b125be0
Teng Lingjing <e1354629@u.nus.edu>
2026-01-23 16:22:01 +0800

    Level-0: Rename chatbot and implement greet/exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Misaka19090 User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Misaka19090.java
@@ -0,0 +1,13 @@
+public class Misaka19090 {
+    public static void main(String[] args) {
+
+
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Misaka19090");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
