28db9ab054b3f499cface0f7de3af9b99dd5a5f9
Teng Lingjing <e1354629@u.nus.edu>
2026-01-23 16:53:03 +0800

    Level-1: Echo user commands until bye

--- src/main/java/Misaka19090.java
+++ src/main/java/Misaka19090.java
@@ -1,13 +1,26 @@
+import java.util.Scanner;
 public class Misaka19090 {
     public static void main(String[] args) {
 
-
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Misaka19090");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
